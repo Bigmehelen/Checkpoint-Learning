e381457547da30eb3ee5601debedc9433132b62f
Helen <getfitbyhelena@gmail.com>
2026-04-17 21:40:30 +0100

    digitlen

--- main.go
+++ main.go
@@ -3,8 +3,5 @@ package main
 import "fmt"
 
 func main(){
-	fmt.Println(RetainFirstHalf("This is the 1st halfThis is the 2nd half"))
-	fmt.Println(RetainFirstHalf("A"))
-	fmt.Println(RetainFirstHalf(""))
-	fmt.Println(RetainFirstHalf("Hello World"))
+    fmt.Printf("%c\n", FirstRune("Hello")) 
 }
