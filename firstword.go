55ade55f4e45729642d963f27333242d9379507e
Helen <getfitbyhelena@gmail.com>
2026-04-17 21:47:50 +0100

    first word

---
+++ firstword.go
@@ -0,0 +1,17 @@
+package main
+
+func FirstWord(s string) string {
+	i := 0
+
+	for i < len(s) && s[i] == ' ' {
+		i++
+	}
+
+	start := i
+
+	for i < len(s) && s[i] != ' ' {
+		i++
+	}
+
+	return s[start:i] + "\n"
+}
