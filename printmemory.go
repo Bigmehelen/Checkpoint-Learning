df8faf5a5e6a10082957d99a14a177818e035067
Helen <getfitbyhelena@gmail.com>
2026-04-29 04:47:46 +0100

    printmemory

--- printmemory.go
+++ printmemory.go
@@ -9,11 +9,17 @@ func PrintMemory(arr [10]byte) {
 		if i != len(arr)-1{
 			z01.PrintRune(' ')
 		}
-		z01.PrintRune(rune(arr[i]))
-		z01.PrintRune('\n')
 	}
 	z01.PrintRune('\n')
-	
+
+	for i := 0; i < len(arr); i++ {
+		if arr[i] >= 32 && arr[i] <= 126 {
+			z01.PrintRune(rune(arr[i]))
+		} else {
+			z01.PrintRune('.')
+		}
+	}
+	z01.PrintRune('\n')
 }
 
 func PrintHex(b byte) {
